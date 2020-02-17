5a989021b32e19edaa4df00a9e5c827e858a2e12
HKEDU <HKEDU@DESKTOP-VEA1FKK>
2020-02-17 11:26:50 +0900

    11

--- sp4-test06/src/main/java/Spring/MemberListPrinter.java
+++ sp4-test06/src/main/java/Spring/MemberListPrinter.java
@@ -3,8 +3,11 @@ package Spring;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class MemberListPrinter {
+	@Autowired
+//	@Resource(name="MemberDAO")
 	private MemberDAO memberDAO;
 	private MemberPrinter printer;
 	@Autowired
@@ -12,6 +15,8 @@ public class MemberListPrinter {
 		this.memberDAO = memberDAO;
 	}
 	@Autowired
+	@Qualifier("sysout")	
+	// 클래스 선언변수가 하나 이상 있어서. 싱글톤이 아닐때. 변수가 두 개일 때. 거기서 선언한 이름name으로 갖다쓰는 거...?
 	public void setPrinter(MemberPrinter printer) {
 		this.printer = printer;
 	}
