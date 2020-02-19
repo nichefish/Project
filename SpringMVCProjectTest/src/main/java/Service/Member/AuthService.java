9190dd413f59e7d571dd8605a707439f59fac85f
HKEDU <HKEDU@DESKTOP-VEA1FKK>
2020-02-19 13:04:47 +0900

    11

--- SpringMVCProjectTest/src/main/java/Service/Member/AuthService.java
+++ SpringMVCProjectTest/src/main/java/Service/Member/AuthService.java
@@ -23,15 +23,23 @@ public class AuthService {
 		member.setUserId(loginCommand.getId1());
 		member.setUserId(Encrypt.getEncryption(loginCommand.getPw()));
 		member.setUserId(loginCommand.getId1());
-		member = MemberDAO.selectByUserId(member);
+		member = memberDAO.selectByUserId(member);
 		try {	// member가 null일 때.. 즉 id가 존재하지 않을 때...
 			authInfo = new AuthInfo(member.getUserId(), member.getUserEmail(), member.getUserName(), member.getUserPw());
 			// 객체 만들고 setter 네 번 쓰는 대신. 객체 만들 때부터 생성자 이용해서 한 번에 초기화...
+			if (authInfo.getPw().equals(Encrypt.getEncryption(loginCommand.getPw()))) {		// 비밀번호가 맞으면..
+				session.setAttribute("authInfo",  authInfo);								// 로그인 정보 세션에 저장...
+			} else {
+				System.out.println("비밀번호가 틀립니다.");
+				errors.rejectValue("pw", "wrong");
+			}
 		} catch (NullPointerException e) {
 			System.out.println("아이디가 없습니다.");
 			errors.rejectValue("id1", "notId");
 		}
-//		if (authInfo.getPw())
+		
+			
+		
 		
 	}
 	public void detailInfo(LoginCommand loginCommand) {
